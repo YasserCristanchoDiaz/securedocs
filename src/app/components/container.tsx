3be09e189efabd75b8c81c58200fa8be2fbb2c74
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2023-07-24 19:15:51 -0500

    Vista principal adelanto

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -6,12 +6,15 @@ export default function Container({children}: {children:React.ReactNode}){
     return (
         <div className="flex justify-content-center align-items-center" style={{ height: '100vh', width: '100vw',
          backgroundImage:`url(${background.src})`, backgroundRepeat:'no-repeat', backgroundSize: 'cover'}}>
-            <div className="flex justify-content-center align-items-center" style={{position: 'fixed', 
+            <div className=" navBar flex justify-content-between align-items-center" style={{position: 'fixed', 
             top:0, right:0, left:0, height: '15vh', width: '100vw', backgroundColor:'#D9E6FA'}}>
-                <h1>SecureDosc</h1>
-                <Button label='Sign up' style={{height:'48px'}}></Button>
-                <Button label='login' style={{height:'48px'}}></Button>
+                <h1  style={{ paddingLeft: '20px' }}>SecureDocs</h1>
+                <div style={{ display: 'flex', gap: '10px', paddingRight: '20px' }}>
+            <Button label='Sign up' style={{ height: '48px' }}></Button>
+            <Button label='Login' style={{ height: '48px' }}></Button>
+        </div>
             </div>
+            
             <div className="md:col-6 col-11 md:p-8 p-3 border-round-3xl" style={{backgroundColor:'RGBA(25, 167,206,0.55)'}}>
                 {children}
             </div>
@@ -19,3 +22,4 @@ export default function Container({children}: {children:React.ReactNode}){
     )
 
 }
+
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -7,16 +7,22 @@ import { useState } from "react"
 import Container from '@/app/components/container';
 
 export default function Home() {
-    const [message, setMessage] = useState<string>('Hola kaka')
+    const [message, setMessage] = useState<string>('¡Bienvenido a SecureDocs! En SecureDocs, nos enorgullece ofrecerte una solución segura y eficiente para gestionar tus documentos en línea. Nuestra plataforma ha sido diseñada pensando en la comodidad y la protección de tu información confidencial.')
 
     const clickBtn = () => {
         setMessage('clickeando... pin')
     }
+
+    const splittedMessage = message.split('!'); 
+    const firstPart = splittedMessage.slice(0, -1).join('!'); 
+    const lastPart = splittedMessage.slice(-1).join('!'); 
     return (
         <Container>
-            <h1>{message}</h1>
+            <h2>{firstPart}!<br />{lastPart}</h2>
+            {/*
             <InputText value={message} onChange={(e)=>setMessage(e.target.value)}></InputText>
             <Button label="Espichame" onClick={clickBtn}></Button>
+    */}
         </Container>
     )
 } 
