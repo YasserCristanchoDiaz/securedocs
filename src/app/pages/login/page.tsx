96ae11c3d13b0ad1bd19225d7726989bb5789f04
YasserCristanchoDiaz <yasser.cristancho@uptc.edu.co>
2023-07-27 21:02:37 -0500

    Obterne user y password desde login

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -4,11 +4,18 @@ import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Container from '@/app/components/container';
 import Link from 'next/link';
 
 export default function Login() {
+    const [user, setUser] = useState<string>('')
+    const [password, setPassword] = useState<string>('') 
+    const [submited, setSubmited] = useState<boolean>()
+    
+    const handleSubmit = () => {
+        console.log('user: ' + user + '\npw: ' + password)
+    }
 
     return (
         <Container title='Inicio de sesion'>
@@ -18,7 +25,7 @@ export default function Login() {
                         <label className="lbl" style={{ fontFamily: 'Roboto, sans-serif', fontSize: '20px', color: '#FFFFFF' }}>Usuario:</label>
                     </div>
                     <div className='col-6'>
-                        <InputText></InputText>
+                        <InputText value={user} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser(e.target.value)}></InputText>
                     </div>
                 </div>
                 <div className="grid justify-content-center mt-2">
@@ -26,14 +33,14 @@ export default function Login() {
                         <label className="lbl" style={{ fontFamily: 'Roboto, sans-serif', fontSize: '20px', color: '#FFFFFF' }}>Contraseña:</label>
                     </div>
                     <div className='col-6'>
-                        <Password></Password>
+                        <Password value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}></Password>
                     </div>
                 </div>
                 <div className='col-6'>
                     <Link href="http://" style={{ fontFamily: 'Roboto, sans-serif', fontSize: '12px', color: '#FFFFFF' }}>Olvide mi contraseña?</Link>
                 </div>
                 <div className='text-center'>
-                    <Button label='Entrar'></Button>
+                    <Button label='Entrar' onClick={handleSubmit}></Button>
                 </div>
             </div>
         </Container>
