f7fbc679f3619a9c2b0b38d646aa77b37d88cc07
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2023-07-28 22:13:27 -0500

    Logo y campos

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -3,45 +3,67 @@
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-
+import { Message } from 'primereact/message';
 import { useEffect, useState } from "react"
 import Container from '@/app/components/container';
-import Link from 'next/link';
-import homeImage from '../../../../public/homeImage.svg'; 
+import Link from 'next/link'; 
 
 export default function Login() {
     const [user, setUser] = useState<string>('')
+    const[isValidUser,setIsValidUser]=useState(true);
     const [password, setPassword] = useState<string>('') 
-    const [submited, setSubmited] = useState<boolean>()
+    const [errorMessage, setErrorMessage] = useState('');
+    const[isValidPassword, setIsValidPassword]=useState(true);
+    const [isFormSubmited, setIsFormSubmited] = useState(false);
     
     const handleSubmit = () => {
-        console.log('user: ' + user + '\npw: ' + password)
+
+        setIsFormSubmited(true);
+        if(!user || !password){
+            if(!user){
+                setIsValidUser(false);
+            }
+            if(!password){
+                setIsValidPassword(false);
+            }
+
+            setErrorMessage('Por favor, completa todos los campos.'); 
+        }else{
+            console.log('user: ' + user + '\npw: ' + password)
+            setErrorMessage('');
+        }
     }
 
     return (
         <Container title='Login' showButtons={false}>
             <div>
-                <div className="grid justify-content-center">
-                    <div className='col-2'>
-                        <label className="labels" >Usuario:</label>
-                    </div>
-                    <div className='col-6'>
-                        <InputText value={user} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser(e.target.value)}></InputText>
-                    </div>
-                </div>
-                <div className="grid justify-content-center mt-1">
-                    <div className='col-2'>
-                        <label className="labels">Contraseña:</label>
-                    </div>
-                    <div className='col-6'>
-                        <Password value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} feedback={false}></Password>
+                <div>
+                    <div className="text-center  my-2">
+                        <span className='p-input-icon-right'>
+                            <i className="pi pi-user" style={{ fontSize: '14px' }}/>
+                        <InputText value={user} className={!isValidUser ? 'p-invalid' : ' ' }  placeholder="Usuario"onChange={(e) =>{
+                                setIsValidUser(true); 
+                                setUser(e.target.value)
+                                }}/>
+                        </span>
                     </div>
                 </div>
-                <div className='col-6'>
+                <div >
+                    <div className="text-center  my-2">
+                        <Password value={password} className={!isValidPassword ? 'p-invalid' : ' ' }  placeholder="Contraseña"feedback={false} onChange={(e) =>{
+                                setIsValidPassword(true); 
+                                setPassword(e.target.value)
+                                }} toggleMask/>
+                    </div>
+                </div>
+                <div className='text-center  my-2'>
                     <Link href="http://" style={{ fontFamily: 'Roboto, sans-serif', fontSize: '14px', color: '#FFFFFF' }}>Olvide mi contraseña?</Link>
                 </div>
-                <div className='text-center'>
+                <div className='text-center  my-2'>
                     <Button label='Entrar' onClick={handleSubmit} style={{backgroundColor:'#146C94', borderColor:'#146C94'}}></Button>
+                </div>
+                <div className="card flex flex-wrap align-items-center justify-content-center gap-3">
+                {errorMessage && (<Message severity="error" text={errorMessage} />)}
                 </div>
             </div>
         </Container>
